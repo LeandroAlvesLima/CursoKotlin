aa39d3bae7459524e943e2a5df00d2ff8afb778a
LeandroAlvesLima <leandroalveslima@outlook.com>
2021-09-01 23:39:47 -0300

    "Criando Classe Pessoa - Curso Kotlin"

---
+++ fundamentos_oo/src/main/kotlin/Pessoa.kt
@@ -0,0 +1,10 @@
+class Pessoa {
+    var name:String = "Leandro"
+    var cpf:String ="123.123.123-12"
+}
+fun main(){
+    val Leandro = Pessoa()
+ println(Leandro.name)
+ println(Leandro.cpf)
+
+}
