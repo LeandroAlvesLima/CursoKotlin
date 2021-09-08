d12561a4ea90dbbe55226e8463c577aa148df124
LeandroAlvesLima <leandroalveslima@outlook.com>
2021-09-08 20:33:15 -0300

    "Criando construtor secundario e chamando uma função para imprimir - Curso Kotlin"

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/Pessoa.kt
@@ -4,11 +4,15 @@ class Pessoa {
     var nome:String = " Leandro "
     var cpf:String = " 123.123.123.-12"
     private set
+
+    constructor()
+
+    fun pessoaInfo( )="$nome + $cpf"
 }
 fun main(){
     val Leandro = Pessoa()
 
-    println(Leandro.nome)
-    println(Leandro.cpf)
+    println(message = Leandro.pessoaInfo())
+
 
 }
