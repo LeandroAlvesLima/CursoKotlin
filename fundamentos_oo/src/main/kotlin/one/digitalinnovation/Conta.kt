0127bc4be31b27ccd213d73595893926155c58b0
LeandroAlvesLima <leandroalveslima@outlook.com>
2021-09-08 20:58:49 -0300

    "Criando  classe Conta - Curso Kotlin"

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/Conta.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/Conta.kt
@@ -2,16 +2,14 @@ package one.digitalinnovation
 
 import java.math.BigDecimal
 
-class Conta {
-    val agencia:String
-    val conta:String
-    val saldo:BigDecimal ;
-}
-
+class Conta (
+    val agencia:String,
+    val conta:String,
+    val saldo:BigDecimal
+    ) {
     fun deposito(valor: BigDecimal) {
-
     }
 
     fun saque(valor: BigDecimal) {
-
     }
+}
